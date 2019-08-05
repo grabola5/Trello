ac953fe0ef07fe96d361d747c8a58c07df032c1e
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-08-05 15:41:32 +0200

    Make complete class Column

--- js/script.js
+++ js/script.js
@@ -31,10 +31,24 @@ document.addEventListener('DOMContentLoaded', function() {
     this.id = randomString();
     this.name = name;
     this.element = generateTemplate('column-template', {name: this.name});
+    this.element.querySelector('.column').addEventListener('click', function (event) {
+      if (event.target.classList.contains('btn-delete')) {
+        self.removeColumn();
+      }
+      if (event.target.classList.contains ('add-card')) {
+        self.addCard( new Card(prompt("Enter the name of the card")));
+      }
+    });
+  };
+//metody dla klasy Column
+  Column.prototype = {
+    addCard: function(card) {
+      this.element.querySelector('ul')appendChild(card.element);
+    },
+    removeColumn: function () {
+      this.element.parentNode.removeChild(this.element);
+    }
   }
-
-
-
 });
 
 })();
